e11afc65167c56b108a668a5a4ff2da1d9d743c6
vgabruzzo <vgabruzzo@gmail.com>
2015-12-23 23:02:25 -0500

    update App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Header, NavTabs, Footer } from '../components';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
+import ThemeDecorator from 'material-ui/lib/styles/theme-decorator';
 
+//@ThemeDecorator(ThemeManager.getMuiTheme(LightRawTheme))
 class App extends Component {
   render() {
-    const { content, dispatch } = this.props;
+    const { content, dispatch, route } = this.props;
 
     return (
       <div className='page'>
-        <Header dispatch={dispatch}/>
+        <Header dispatch={dispatch} route={route} />
 				<div className="content">
           {content}
 				</div>
@@ -18,4 +22,12 @@ class App extends Component {
   }
 }
 
-export default connect()(App)
+function mapStateToProps(state) {
+  return {
+		route: state.routing
+  };
+}
+
+export default connect(
+ mapStateToProps
+)(App)
