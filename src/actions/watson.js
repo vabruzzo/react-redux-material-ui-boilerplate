d2a2667da16e64dec9d71c94029dc94d27b66e8d
vgabruzzo <vgabruzzo@gmail.com>
2015-12-23 23:01:37 -0500

    add actions

--- src/actions/watson.js
+++ src/actions/watson.js
@@ -1,1 +1,13 @@
 import * as types from '../constants/ActionTypes';
+
+export function requestAnswer() {
+  return {
+    type: types.INCREMENT_COUNTER
+  }
+}
+
+export function receiveAnswer() {
+  return {
+    type: types.DECREMENT_COUNTER
+  }
+}
