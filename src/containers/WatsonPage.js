f4fc5c8a2b328959d958dd6fc1e97b1c077c3876
vgabruzzo <vgabruzzo@gmail.com>
2015-12-23 23:02:52 -0500

    update watson

--- src/containers/WatsonPage.js
+++ src/containers/WatsonPage.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/lib/text-field';
+import Paper from 'material-ui/lib/paper'
+import RaisedButton from 'material-ui/lib/raised-button';
+import CircularProgress from 'material-ui/lib/circular-progress';
 
 export default class WatsonPage extends Component {
   render() {
     return (
-      <div className="watson">
-        <p>WatsonSearch!!</p>
+      <div>
+        <Paper zDepth="2">
+          <p>Ask IBM's Watson a question.</p>
+          <TextField
+            hintText="question"
+            floatingLabelText="ask watson:" />
+          <RaisedButton label="Ask!" />
+          <div></div>
+        </Paper>
       </div>
     );
   }
