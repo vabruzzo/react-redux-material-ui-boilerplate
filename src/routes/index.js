31d6fcb704e6824827f1481d86e83f8d29ce94cc
vgabruzzo <vgabruzzo@gmail.com>
2015-12-23 23:03:41 -0500

    rm /about

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ export default function getRoutes() {
   return (
     <Route path="/" component={App}>
       <IndexRoute components={{content: MainPage}}/>
-      <Route path="about" components={{content: MainPage}}/>
       <Route path="counter" components={{content: CounterPage}}/>
       <Route path="watson" components={{content: WatsonPage}}/>
     </Route>
