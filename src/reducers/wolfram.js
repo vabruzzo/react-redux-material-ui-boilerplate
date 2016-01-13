1aa3cb6ff5700d5657bc53899501f5806800e7cc
Vincent Abruzzo <vabruzzo@hampshire.edu>
2016-01-13 15:44:47 -0500

    add reducer

---
+++ src/reducers/wolfram.js
@@ -0,0 +1,32 @@
+import { REQUEST_ANSWER, RECEIVE_ANSWER } from '../constants/ActionTypes'
+
+function answer(state = {
+  isFetching: false,
+  items: []
+}, action) {
+  switch (action.type) {
+    case REQUEST_ANSWER:
+      return Object.assign({}, state, {
+        isFetching: true
+      })
+    case RECEIVE_ANSWER:
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: action.posts,
+      })
+    default:
+      return state
+  }
+}
+
+function answerFromWolfram(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_ANSWER:
+    case REQUEST_ANSWER:
+      return Object.assign({}, state, {
+        [action.reddit]: answer(state[action.reddit], action)
+      })
+    default:
+      return state
+  }
+}
